4a65714306fe6c0849f01c32fc19c93ed22248d3
Satvik <satvikpassey22@gmail.com>
2023-06-09 17:01:48 +0530

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from datetime import date, datetime
+import urllib.request
+import bs4 as bs
+import json
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+from flask import Flask, render_template, request
 from flask import Flask, request, render_template, request, session, redirect
 import pickle
 import requests
@@ -5,17 +12,28 @@ import pandas as pd
 from patsy import dmatrices
 import sqlite3
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'db-mysql-blr1-20896-do-user-14202162-0.b.db.ondigitalocean.com'
-app.config['MYSQL_USER'] = 'doadmin'
-app.config['MYSQL_PASSWORD'] ='AVNS_Ui6vyIsEz27YkcxrwPe'
-app.config['MYSQL_DB'] = 'test'
-app.config['MYSQL_PORT']='25060'
+load_dotenv()
+
+HOST=os.getenv("HOST")
+USER=os.getenv("USER")
+PASSWORD=os.getenv("PASSWORD")
+DATABASE=os.getenv("DATABASE")
+PORT=os.getenv("PORT")
+
+app.config['MYSQL_HOST'] = HOST
+app.config['MYSQL_USER'] = USER
+app.config['MYSQL_PASSWORD'] =PASSWORD
+app.config['MYSQL_DB'] = DATABASE
+app.config['MYSQL_PORT'] = PORT
 
 mysql = MySQL(app)
 app.secret_key = '17041973984'
+
 
 @app.route('/', methods=['GET'])
 def index():
@@ -123,59 +141,54 @@ def contact():
     return render_template('contact.html')
 
 
-import numpy as np
-import pandas as pd
-from flask import Flask, render_template, request
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import json
-import bs4 as bs
-import urllib.request
-import pickle
-import requests
-from datetime import date, datetime
-
 # load the nlp model and tfidf vectorizer from disk
 filename = 'nlp_model.pkl'
 clf = pickle.load(open(filename, 'rb'))
-vectorizer = pickle.load(open('tranform.pkl','rb'))
-    
+vectorizer = pickle.load(open('tranform.pkl', 'rb'))
+
 # converting list of string to list (eg. "["abc","def"]" to ["abc","def"])
+
+
 def convert_to_list(my_list):
     my_list = my_list.split('","')
-    my_list[0] = my_list[0].replace('["','')
-    my_list[-1] = my_list[-1].replace('"]','')
+    my_list[0] = my_list[0].replace('["', '')
+    my_list[-1] = my_list[-1].replace('"]', '')
     return my_list
 
 # convert list of numbers to list (eg. "[1,2,3]" to [1,2,3])
+
+
 def convert_to_list_num(my_list):
     my_list = my_list.split(',')
-    my_list[0] = my_list[0].replace("[","")
-    my_list[-1] = my_list[-1].replace("]","")
+    my_list[0] = my_list[0].replace("[", "")
+    my_list[-1] = my_list[-1].replace("]", "")
     return my_list
+
 
 def get_suggestions():
     data = pd.read_csv('main_data.csv')
     return list(data['movie_title'].str.capitalize())
 
+
 @app.route("/home")
 def home():
     suggestions = get_suggestions()
-    return render_template('home.html',suggestions=suggestions)
+    return render_template('home.html', suggestions=suggestions)
 
-@app.route("/populate-matches",methods=["POST"])
+
+@app.route("/populate-matches", methods=["POST"])
 def populate_matches():
     # getting data from AJAX request
-    res = json.loads(request.get_data("data"));
-    movies_list = res['movies_list'];
+    res = json.loads(request.get_data("data"))
+    movies_list = res['movies_list']
 
-    movie_cards = {"https://image.tmdb.org/t/p/original"+movies_list[i]['poster_path'] if movies_list[i]['poster_path'] else "/static/movie_placeholder.jpeg": [movies_list[i]['title'],movies_list[i]['original_title'],movies_list[i]['vote_average'],datetime.strptime(movies_list[i]['release_date'], '%Y-%m-%d').year if movies_list[i]['release_date'] else "N/A", movies_list[i]['id']] for i in range(len(movies_list))}
+    movie_cards = {"https://image.tmdb.org/t/p/original"+movies_list[i]['poster_path'] if movies_list[i]['poster_path'] else "/static/movie_placeholder.jpeg": [movies_list[i]['title'], movies_list[i]['original_title'],
+                                                                                                                                                                movies_list[i]['vote_average'], datetime.strptime(movies_list[i]['release_date'], '%Y-%m-%d').year if movies_list[i]['release_date'] else "N/A", movies_list[i]['id']] for i in range(len(movies_list))}
 
-    return render_template('recommend.html',movie_cards=movie_cards);
+    return render_template('recommend.html', movie_cards=movie_cards)
 
 
-
-@app.route("/recommend",methods=["POST"])
+@app.route("/recommend", methods=["POST"])
 def recommend():
     # getting data from AJAX request
     title = request.form['title']
@@ -216,35 +229,40 @@ def recommend():
     cast_bdays = convert_to_list(cast_bdays)
     cast_bios = convert_to_list(cast_bios)
     cast_places = convert_to_list(cast_places)
-    
+
     # convert string to list (eg. "[1,2,3]" to [1,2,3])
     cast_ids = convert_to_list_num(cast_ids)
     rec_vote = convert_to_list_num(rec_vote)
     rec_year = convert_to_list_num(rec_year)
     rec_ids = convert_to_list_num(rec_ids)
-    
+
     # rendering the string to python string
     for i in range(len(cast_bios)):
-        cast_bios[i] = cast_bios[i].replace(r'\n', '\n').replace(r'\"','\"')
+        cast_bios[i] = cast_bios[i].replace(r'\n', '\n').replace(r'\"', '\"')
 
     for i in range(len(cast_chars)):
-        cast_chars[i] = cast_chars[i].replace(r'\n', '\n').replace(r'\"','\"') 
-    
+        cast_chars[i] = cast_chars[i].replace(r'\n', '\n').replace(r'\"', '\"')
+
     # combining multiple lists as a dictionary which can be passed to the html file so that it can be processed easily and the order of information will be preserved
-    movie_cards = {rec_posters[i]: [rec_movies[i],rec_movies_org[i],rec_vote[i],rec_year[i],rec_ids[i]] for i in range(len(rec_posters))}
+    movie_cards = {rec_posters[i]: [rec_movies[i], rec_movies_org[i],
+                                    rec_vote[i], rec_year[i], rec_ids[i]] for i in range(len(rec_posters))}
 
-    casts = {cast_names[i]:[cast_ids[i], cast_chars[i], cast_profiles[i]] for i in range(len(cast_profiles))}
+    casts = {cast_names[i]: [cast_ids[i], cast_chars[i],
+                             cast_profiles[i]] for i in range(len(cast_profiles))}
 
-    cast_details = {cast_names[i]:[cast_ids[i], cast_profiles[i], cast_bdays[i], cast_places[i], cast_bios[i]] for i in range(len(cast_places))}
+    cast_details = {cast_names[i]: [cast_ids[i], cast_profiles[i], cast_bdays[i],
+                                    cast_places[i], cast_bios[i]] for i in range(len(cast_places))}
 
-    if(imdb_id != ""):
+    if (imdb_id != ""):
         # web scraping to get user reviews from IMDB site
-        sauce = urllib.request.urlopen('https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt'.format(imdb_id)).read()
-        soup = bs.BeautifulSoup(sauce,'lxml')
-        soup_result = soup.find_all("div",{"class":"text show-more__control"})
+        sauce = urllib.request.urlopen(
+            'https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt'.format(imdb_id)).read()
+        soup = bs.BeautifulSoup(sauce, 'lxml')
+        soup_result = soup.find_all(
+            "div", {"class": "text show-more__control"})
 
-        reviews_list = [] # list of reviews
-        reviews_status = [] # list of comments (good or bad)
+        reviews_list = []  # list of reviews
+        reviews_status = []  # list of comments (good or bad)
         for reviews in soup_result:
             if reviews.string:
                 reviews_list.append(reviews.string)
@@ -257,21 +275,22 @@ def recommend():
         # getting current date
         movie_rel_date = ""
         curr_date = ""
-        if(rel_date):
+        if (rel_date):
             today = str(date.today())
-            curr_date = datetime.strptime(today,'%Y-%m-%d')
+            curr_date = datetime.strptime(today, '%Y-%m-%d')
             movie_rel_date = datetime.strptime(rel_date, '%Y-%m-%d')
 
         # combining reviews and comments into a dictionary
-        movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))}     
+        movie_reviews = {reviews_list[i]: reviews_status[i]
+                         for i in range(len(reviews_list))}
 
         # passing all the data to the html file
-        return render_template('recommend.html',title=title,poster=poster,overview=overview,vote_average=vote_average,
-            vote_count=vote_count,release_date=release_date,movie_rel_date=movie_rel_date,curr_date=curr_date,runtime=runtime,status=status,genres=genres,movie_cards=movie_cards,reviews=movie_reviews,casts=casts,cast_details=cast_details)
+        return render_template('recommend.html', title=title, poster=poster, overview=overview, vote_average=vote_average,
+                               vote_count=vote_count, release_date=release_date, movie_rel_date=movie_rel_date, curr_date=curr_date, runtime=runtime, status=status, genres=genres, movie_cards=movie_cards, reviews=movie_reviews, casts=casts, cast_details=cast_details)
 
     else:
-        return render_template('recommend.html',title=title,poster=poster,overview=overview,vote_average=vote_average,
-            vote_count=vote_count,release_date=release_date,movie_rel_date="",curr_date="",runtime=runtime,status=status,genres=genres,movie_cards=movie_cards,reviews="",casts=casts,cast_details=cast_details)
+        return render_template('recommend.html', title=title, poster=poster, overview=overview, vote_average=vote_average,
+                               vote_count=vote_count, release_date=release_date, movie_rel_date="", curr_date="", runtime=runtime, status=status, genres=genres, movie_cards=movie_cards, reviews="", casts=casts, cast_details=cast_details)
 
 
 if __name__ == '__main__':
