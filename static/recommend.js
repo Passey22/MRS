a46da12b2cd8de899017f3c468078e798a328ed6
Satvik <satvikpassey22@gmail.com>
2023-06-21 23:30:51 +0530

    recommend.js

--- static/recommend.js
+++ static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-   var my_api_key = 'API_KEY';
+   var my_api_key = '2624cdab70a28578cbc7d0f0f7c729b4';
    var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){ 
-  var my_api_key = 'API_KEY';
+  var my_api_key = '2624cdab70a28578cbc7d0f0f7c729b4';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
